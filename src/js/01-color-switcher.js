a1febdc061fec6f6a322b7877827de471df19e74
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-09-26 14:51:27 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const refs = {
+    startButton: document.querySelector('[data-start]'),
+    stopButton: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+let timerId = null;
+
+
+refs.startButton.addEventListener('click', (e) =>{
+    console.log(e.target);
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+
+      if(refs.startButton.classList.contains('activated')) return;
+        refs.stopButton.classList.remove('activated');
+        refs.startButton.classList.add('activated');
+       
+});
+
+refs.stopButton.addEventListener('click', (e) =>{
+    clearInterval(timerId);
+    console.log(`Interval with id ${timerId} has stopped!`);
+    if(refs.stopButton.classList.contains('activated')) return;
+        refs.startButton.classList.remove('activated');
+        refs.stopButton.classList.add('activated');
+        
+});
