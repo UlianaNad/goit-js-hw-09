ca5d5d1d3df5fa6b753a3d202656f5a33282ebd7
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-09-28 16:12:48 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const options ={
+  width: '320px',
+  timeout: 10000,
+}
+const formEl = document.querySelector('.form');
+
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const {elements: { delay, step, amount }} = e.currentTarget;
+
+  for(let i = 0; i < amount.value; i += 1){
+
+    let position = i + 1;
+    const delayValue = Number(delay.value) + step.value * i;
+
+    createPromise(position, delayValue)
+    .then(({position, delay}) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms!`, options)
+    })
+    .catch(({position, delay}) => {
+      Notify.warning(`❌ Rejected promise ${position} in ${delay}ms!`, options)
+    })
+  }
+});
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
 }
